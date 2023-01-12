346dbb90d418acd354395be2b5249db105b300b6
clay hendrix <clayhendrix5@gmail.com>
2023-01-11 19:10:07 -0600

    fixed title

--- script.js
+++ script.js
@@ -17,9 +17,17 @@ const menuToggle = document.getElementById('navbarSupportedContent')
 navLinks.forEach((l) => {
     l.addEventListener('click', () => { new bootstrap.Collapse(menuToggle).toggle() })
 })
-// Timeout if off my page for too long
-var title = document.querySelector("title");
+// Store the original title of the webpage
+var originalTitle = document.querySelector("title").textContent;
 
+// Update the title of the webpage using setTimeout
 setTimeout(function(){
-  title.textContent = "Please come back- Clay";
+  document.querySelector("title").textContent = "Please come back- Clay";
 }, 5000);
+
+// Check for visibility changes and restore the original title when the page becomes visible again
+document.addEventListener("visibilitychange", function() {
+    if (!document.hidden) {
+      document.querySelector("title").textContent = originalTitle;
+    }
+});
